adf5e3c831d165c3d3842f9b3f05116d3fe35b62
Miguel Ortiz <mickyor97@gmail.com>
2023-05-04 17:52:09 -0400

    Soluciones dia 3

---
+++ CodigoAuxiliar/SolucionesDia3_02052023/A.cpp
@@ -0,0 +1,78 @@
+/*
+               __
+              / _)
+     _.----._/ /
+    /         /
+ __/ (  | (  |
+/__.-'|_|--|_|
+*/
+#include <bits/stdc++.h>
+ //#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include <bits/extc++.h>
+typedef long long ll;
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define all(a) (a).begin(), (a).end()
+#define clr(a, h) memset(a, (h), sizeof(a))
+#define f first
+#define s second
+#define fore(i, b, e) for (int i = (int) b, o_o = e; i < (int) o_o; ++i)
+#define forr(i, b, e) for (int i = (int) b, o_o = e; i < (int) o_o; ++i)
+#define deb(x) cerr << "# " << (#x) << " = " << (x) << endl;
+#define sz(x) (int) x.size()
+#define endl '\n'
+// int in(){int r=0,c;for(c=getchar();c<=32;c=getchar());if(c=='-') return -in();for(;c>32;r=(r<<1)+(r<<3)+c-'0',c=getchar());return r;}
+
+using namespace std;
+//using namespace __gnu_pbds;
+//#pragma GCC target ("avx2")
+//#pragma GCC optimization ("O3")
+//#pragma GCC optimization ("unroll-loops")
+
+typedef pair < int, int > ii;
+typedef vector < int > vi;
+typedef vector < ii > vii;
+typedef vector < ll > vll;
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+//find_by_order kth largest  order_of_key <
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const int INF = numeric_limits < int > ::max() / 4;
+const double PI = acos(-1);
+
+signed main() {
+  std::ios::sync_with_stdio(false);
+  cin.tie(0);
+  //freopen("","r",stdin);
+  //freopen("","w",stdout);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vi divs;
+    for (int i = 1; i*i <= n; ++i) {
+      if (n%i == 0) {
+        divs.pb(i);
+        divs.pb(n / i);
+      }
+    }
+    int a = 1, b = n-1;
+    int best = n-1;
+    for (int d : divs) {
+      int x = d;
+      int y = n - d;
+      if (x == 0 || y == 0) continue;
+      int lcm = x / __gcd(x, y) * y;
+      if (lcm < best) {
+        best = lcm;
+        a = x;
+        b = y;
+      }
+    }
+    cout << a << " " << b << endl;
+  }
+  return 0;
+}
